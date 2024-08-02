2282b322812761e8a79945c271ad7f4226be96c6
Mohmmed.info <124050324+welecomtopython@users.noreply.github.com>
2024-08-02 05:19:59 +0300

    رفع مجلد كامل

---
+++ loc/progrmming/app/__init__.py
@@ -0,0 +1,14 @@
+
+from app.add import Add
+from app.config import Config
+from app.get import Get
+from app.init import *
+from app.set import *
+
+__all__=[
+    'Add',
+    'Config',
+    'Get',
+    'Init',
+    "Set"
+]
