b2e4cdcc3ded649ba0957a45b2c4cb84273bed92
Mohmmed.info <124050324+welecomtopython@users.noreply.github.com>
2024-08-02 05:26:50 +0300

    رفع مجلد كامل

---
+++ progrmming/app/add/__init__.py
@@ -0,0 +1,13 @@
+class Add:
+    def __init__(self, args):
+        """
+           A class to handle addition of new items to a folder .
+
+           Attributes:
+               args (List[Tuple]): A list of tuples where each tuple represents an item to be added.
+           """
+        print("Add initialized with args:", args)
+
+
+
+
